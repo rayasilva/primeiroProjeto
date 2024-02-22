61d11af080d55e971b4199a3cdbcf117ba349fb1
rayasilva <rayane2.pereira1b1@gmail.com>
2024-02-22 10:41:33 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Rayane Pereira da Silva </Text>
+      <Text>Desenvolvimento de Dispositivos Moveis 22/fev2024</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
