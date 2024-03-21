a8a91121579b0ced7445ace56ffdf7f4d1f8722f
rayasilva <rayane2.pereira1b1@gmail.com>
2024-03-21 09:48:58 -0300

    Primeira Prova DDM lado B

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Title(){
+export default function Rayane(){
     return(
     <View>
-        <Text>Titulo 3B1 Lado B</Text>
+        <Text>Rayane Pereira da Silva </Text>
+        <Text>Primeira Prova DDM lado B</Text>
     </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
